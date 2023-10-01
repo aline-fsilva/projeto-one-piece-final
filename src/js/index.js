10769fcfd91685f873ae00893fc3829e789674cc
Aline Ferreira da Silva <146677673+aline-fsilva@users.noreply.github.com>
2023-10-01 19:57:46 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+const botoes = document.querySelectorAll('.botao');
+
+const personagens = document.querySelectorAll(".personagem")
+
+botoes.forEach((botao, index) => {
+    botao.addEventListener("click", () => {
+        desselecionadoBotao();
+        desselecionarPersonagem();
+
+        botao.classList.add("selecionado");
+        personagens[index].classList.add("selecionado");
+    });
+})
+
+function desselecionarPersonagem() {
+    const personagemSelecionado = document.querySelector(".personagem.selecionado");
+    personagemSelecionado.classList.remove("selecionado");
+}
+
+function desselecionadoBotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
